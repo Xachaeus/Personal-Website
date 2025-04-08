ac6114e7c9b25b842e1f1c59c4a0be518f740104
Zachariah Sollenberger <zacksoll@udel.edu>
2025-04-08 16:51:45 -0400

    Began working on menu and animations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './Style/app.css';
+
+
+
+function DropdownMenu() {
+  const [open, setOpen] = React.useState(false);
+  const [firstOpen, setFirstOpen] = React.useState(false);
+  return (
+    <div>
+      <div className="DropdownMenuButtonParent">
+        <button className="DropdownMenuButton" onClick={() => {setOpen(!open); setFirstOpen(true)}}>...</button>
+        <p>Menu</p>
+      </div>
+      <div className={open? "DropdownMenuContainerVisible" : (firstOpen? "DropdownMenuContainerInvisible" : "Invisible")}>
+        <p>This is some example text!</p>
+      </div>
+    </div>
+  )
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="HomeBackground">
+      <header className="HeaderBar">
+        <DropdownMenu/>
+        
+        <p>Zachariah Sollenberger</p>
+        
       </header>
+      <p>This site is still in production; please come back later to see the completed site!</p>
     </div>
   );
 }
