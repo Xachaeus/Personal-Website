cc77a14fa3d343d18405c02d0dab3e5040cb2198
Zachariah Sollenberger <zacksoll@udel.edu>
2025-04-09 13:35:56 -0400

    Added picture and blur effect to header

--- src/App.js
+++ src/App.js
@@ -10,16 +10,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
       <BrowserRouter>
+        <HeaderBar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
         </Routes>
-        <HeaderBar/>
       </BrowserRouter>
-      
-    </div>
   );
 }
 
