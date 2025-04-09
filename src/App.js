c73fa86ef650e19b225da950e3e0b1ecd4ea03f4
Zachariah Sollenberger <zacksoll@udel.edu>
2025-04-09 15:12:10 -0400

    Set up blur effect for dropdown menu and added additional pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 import './Style/app.css';
-import { HeaderBar } from './Components/HeaderBar.js';
+import { DropdownMenu, HeaderBar } from './Components/HeaderBar.js';
 import { Home } from './Pages/Home.js';
 import { About } from './Pages/About.js';
+import { Projects } from './Pages/Projects.js';
+import { Project1 } from './Pages/Project1.js';
+import { Project2 } from './Pages/Project2.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
@@ -11,10 +14,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
       <BrowserRouter>
+        <DropdownMenu/>
         <HeaderBar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+            <Route path="/projects/project1" element={<Project1/>}/>
+            <Route path="/projects/project2" element={<Project2/>}/>
         </Routes>
       </BrowserRouter>
   );
